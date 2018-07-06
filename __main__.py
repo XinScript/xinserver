c4f1ba1f98261159abb652f472f98b449e9feb49
XinScript <hobsonchan1992@gmail.com>
2018-07-06 10:58:56 +0100

    clean dir

--- __main__.py
+++ __main__.py
@@ -5,5 +5,6 @@ if len(sys.argv) != 4:
     print('Please make sure the arguments correct:$hostname $port $dirpath')
 else:
     host, port, dirpath = sys.argv[1], int(sys.argv[2]), sys.argv[3]
+    print(host,port,dirpath)
     server = Server(host, port, dirpath)
     server.start()
